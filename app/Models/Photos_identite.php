eae70727ad6922f4d2dd4ad12ac0bef7ff3ee490
OUIJDANE <wijdanechafik07@gmail.com>
2022-11-29 15:06:58 +0100

    first-commit

---
+++ app/Models/Photos_identite.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Photos_identite extends Model
+{
+    use HasFactory;
+}
